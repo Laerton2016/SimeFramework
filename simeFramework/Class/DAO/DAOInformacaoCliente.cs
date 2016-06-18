e677ae43f0caf6b02830f1c536b338126ecb0502
Laerton Marques de Figueiredo <laerton281003@hotmail.com>
2016-06-17 21:39:51 -0300

    Removido as classes para criação de novas

--- simeFramework/Class/DAO/DAOInformacaoCliente.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SIME.Class.DAO
-{
-    public class DAOInformacaoCliente
-    {
-    }
-}
--- simeFramework/Class/EnunAlinhamentos.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace SIME.Class
-{
-    public enum EnunAlinhamentos { LEFT, RIGHT, CENTER };
-}
